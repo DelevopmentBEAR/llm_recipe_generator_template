95a10e9615d8b170f2f016f4c6cf9b1c33236862
Berit Haugen <movies.tv.and.music@gmail.com>
2026-03-20 13:59:01 -0500

    Web AI integration Recipe App - Creation

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,24 @@ const express = require('express')
 const router = express.Router()
 const generateRecipe = require('../services/generate_recipe')
 
+// home page here
 router.get('/', function(req, res, next){
     res.render('enter_ingredients')
 })
 
+// generates LLM request
 router.post('/generate_recipe', function(req, res, next){
-    // TODO complete this method 
+    let formData = req.body
+    let userIngredients = formData.ingredients
+    console.log('User entered:', userIngredients)
+
+    // Make a Gemini request
+    generateRecipe(userIngredients).then(recipeJSON => {
+        return res.render('recipe_result', {userIngredients: userIngredients, recipeJSON: recipeJSON})
+    }).catch(err => { // Catches any errors
+        return next(err)
+    })
+
 })
 
 module.exports = router
--- services/generate_recipe.js
+++ services/generate_recipe.js
@@ -1,1 +1,57 @@
 // TODO complete this file 
+// LLM request here
+
+let { GoogleGenAI, Type } = require('@google/genai')
+
+let genAI = new GoogleGenAI( {} )
+
+//let userInput = 'eggs and broccoli and leftover chicken'
+
+function generateRecipe(ingredients) {
+
+    let prompt = `Suggest one recipe that uses these ingredients
+    Ingredients: ${ingredients}` 
+
+
+    return genAI.models.generateContent( {
+        model: 'gemini-2.5-flash',
+        contents: prompt,
+        config: {
+            systemInstruction: `You are a recipe suggestion bot for a health-conscious, 
+            budget-friendly website. Suggest recipes that are low cost and use healthy ingredients. 
+            You don't have to use all the ingredients the user enters, 
+            especially if they enter a lot of ingredients, but use as many as possible. 
+            Suggest one recipe that uses the ingredients in the prompt.`, 
+            responseMimeType: `application/json`,
+            responseSchema: {
+                type: Type.OBJECT,
+                properties: {
+                    recipeName: {
+                        type: Type.STRING
+                    },
+                    description: {
+                        type: Type.STRING
+                    },
+                    ingredients: {
+                        type: Type.ARRAY,
+                        items: {
+                            type: Type.STRING
+                        }
+                    },
+                    instructions: {
+                        type: Type.ARRAY,
+                        items: {
+                            type: Type.STRING
+                        }
+                    }
+                }
+            }
+        }
+    } ).then( response => {
+        console.log(response.text)
+        let recipe = JSON.parse(response.text)
+        return recipe
+    })
+}
+
+module.exports = generateRecipe
